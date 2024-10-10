6b13f25650485f4223029edb0564ae9afec6e2c6
ngdev-prog <nguidjolmartin4+1@gmail.com>
2024-10-10 14:30:19 +0100

    Update home message
    the message will vary depending on the time at which the user is currently logged in

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -7,16 +7,29 @@ const Home = () => {
     hour: '2-digit',
     minute: '2-digit',
   });
+
   const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
     now,
   );
+
+  const getGreetingMessage = () => {
+    const currentHour = now.getHours();
+
+    if (currentHour < 12) {
+      return 'Good Morning';
+    } else if (currentHour < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  };
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+            {`${getGreetingMessage()}`}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
